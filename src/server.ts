35a3a61ed558c3001d57749b3f42ccfbb5e7264b
ramalhov <guilherme@virdee.co>
2021-08-29 23:10:03 -0300

    Adding dotenv variables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import './config/bootstrap';
 import pusher from './services/pusher';
 
 const app = express();
--- src/services/pusher.ts
+++ src/services/pusher.ts
@@ -1,10 +1,18 @@
 import Pusher from 'pusher';
 
+const {
+  PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER,
+} = process.env;
+
+if (!PUSHER_APP_ID || !PUSHER_KEY || !PUSHER_SECRET || !PUSHER_CLUSTER) {
+  throw new Error('Pusher .env variables are required but they are not available');
+}
+
 const pusher = new Pusher({
-  appId: '1257636',
-  key: '2c44b5b14683ca33b856',
-  secret: '3f4df93f7910c25e0d36',
-  cluster: 'us2',
+  appId: PUSHER_APP_ID,
+  key: PUSHER_KEY,
+  secret: PUSHER_SECRET,
+  cluster: PUSHER_CLUSTER,
   useTLS: true,
 });
 
